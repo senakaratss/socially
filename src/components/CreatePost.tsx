639654527fd3cf4dd8a4ad0d7c78e651967d74db
senakaratss <karatasssena@gmail.com>
2025-09-06 02:08:43 +0300

    image upload feature added

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -9,6 +9,7 @@ import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 
 import { createPost } from "@/actions/post.action";
 import toast from "react-hot-toast";
+import ImageUpload from "./ImageUpload";
 
 function CreatePost() {
   const { user } = useUser();
@@ -55,6 +56,18 @@ function CreatePost() {
             ></Textarea>
           </div>
           {/*TODO: HANDLE IMAGE UPLOAD */}
+          {(showImageUpload || imageUrl) && (
+            <div className="border rounded-lg p-4">
+              <ImageUpload
+                endpoint="postImage"
+                value={imageUrl}
+                onChange={(url) => {
+                  setImageUrl(url);
+                  if (!url) setShowImageUpload(false);
+                }}
+              />
+            </div>
+          )}
 
           <div className="flex items-center justify-between border-t pt-4">
             <div className="flex space-x-2">
